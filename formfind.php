cac60041f4c705799546e6e3848b02a94e90b049
binarymaster <x86corez@gmail.com>
2015-07-11 19:21:25 +0300

    Update

---
+++ formfind.php
@@ -0,0 +1,7 @@
+<form enctype="multipart/form-data" action="find.php" method="POST">
+    pass: <input name="pass" type="text" /><br>
+    BSSID: <input name="bssid" type="text" /><br>
+    ESSID: <input name="essid" type="text" /><br>
+	
+    <input type="submit" value="Find" />
+</form>
