e5f92d55a7ebb5081dfbce2efa3646b0c7739209
Anton Kokarev <github@ogurcom.ru>
2015-07-16 00:33:41 +0500

    Добавил таблицы
    + ESSID, Security, WiFiKey, WPSPIN, WANGateway, DNS

--- stat.php
+++ stat.php
@@ -2,6 +2,13 @@
 $topauth=100;
 $topname=30;
 $topbssid=30;
+$topessid=30;
+$topSecurity=30;
+$topWiFiKey=30;
+$topWPSPIN=30;
+$topWANGateway=30;
+$topDNS=30;
+
 
 echo '<html><head>
 <title>3WiFi</title>
@@ -128,6 +135,138 @@ echo '<html><head>
 		$res->close();
 	};
 	
+	/* Таблица ESSID */
+	
+	$query="SELECT COUNT(DISTINCT `ESSID`) FROM `free`";
+	if ($res = $db->query($query)) {
+		$row = $res->fetch_row();
+		$yessid=$row[0];
+		$res->close();
+	};
+	
+	$query="SELECT `ESSID`, COUNT(*) FROM free GROUP BY `ESSID` ORDER BY COUNT(*) DESC LIMIT $topessid";
+	if ($res = $db->query($query)) {
+		echo "<table>";
+		printf("<tr><th>count(%s)</th><th>ESSID (top$topessid)</th></tr>\n",$yessid);
+		while ($row = $res->fetch_row()) {
+			$xessid=$row[0];
+			$xessidcount=$row[1];
+			printf("<tr><td>%s</td><td>%s</td></tr>\n", $xessidcount, $xessid);
+		};
+		echo "</table>";
+		$res->close();
+	};
+
+	/* Таблица Security */
+	
+	$query="SELECT COUNT(DISTINCT `Security`) FROM `free`";
+	if ($res = $db->query($query)) {
+		$row = $res->fetch_row();
+		$ySecurity=$row[0];
+		$res->close();
+	};
+	
+	$query="SELECT `Security`, COUNT(*) FROM free GROUP BY `Security` ORDER BY COUNT(*) DESC LIMIT $topSecurity";
+	if ($res = $db->query($query)) {
+		echo "<table>";
+		printf("<tr><th>count(%s)</th><th>Security (top$topSecurity)</th></tr>\n",$ySecurity);
+		while ($row = $res->fetch_row()) {
+			$xSecurity=$row[0];
+			$xSecuritycount=$row[1];
+			printf("<tr><td>%s</td><td>%s</td></tr>\n", $xSecuritycount, $xSecurity);
+		};
+		echo "</table>";
+		$res->close();
+	};
+
+	/* Таблица WiFiKey */
+	
+	$query="SELECT COUNT(DISTINCT `WiFiKey`) FROM `free`";
+	if ($res = $db->query($query)) {
+		$row = $res->fetch_row();
+		$yWiFiKey=$row[0];
+		$res->close();
+	};
+	
+	$query="SELECT `WiFiKey`, COUNT(*) FROM free GROUP BY `WiFiKey` ORDER BY COUNT(*) DESC LIMIT $topWiFiKey";
+	if ($res = $db->query($query)) {
+		echo "<table>";
+		printf("<tr><th>count(%s)</th><th>WiFiKey (top$topWiFiKey)</th></tr>\n",$yWiFiKey);
+		while ($row = $res->fetch_row()) {
+			$xWiFiKey=$row[0];
+			$xWiFiKeycount=$row[1];
+			printf("<tr><td>%s</td><td>%s</td></tr>\n", $xWiFiKeycount, $xWiFiKey);
+		};
+		echo "</table>";
+		$res->close();
+	};
 
 	
+	/* Таблица WPSPIN */
+	
+	$query="SELECT COUNT(DISTINCT `WPSPIN`) FROM `free`";
+	if ($res = $db->query($query)) {
+		$row = $res->fetch_row();
+		$yWPSPIN=$row[0];
+		$res->close();
+	};
+	
+	$query="SELECT `WPSPIN`, COUNT(*) FROM free GROUP BY `WPSPIN` ORDER BY COUNT(*) DESC LIMIT $topWPSPIN";
+	if ($res = $db->query($query)) {
+		echo "<table>";
+		printf("<tr><th>count(%s)</th><th>WPSPIN (top$topWPSPIN)</th></tr>\n",$yWPSPIN);
+		while ($row = $res->fetch_row()) {
+			$xWPSPIN=$row[0];
+			$xWPSPINcount=$row[1];
+			printf("<tr><td>%s</td><td>%s</td></tr>\n", $xWPSPINcount, $xWPSPIN);
+		};
+		echo "</table>";
+		$res->close();
+	};
+
+	
+	/* Таблица WANGateway */
+	
+	$query="SELECT COUNT(DISTINCT `WANGateway`) FROM `free`";
+	if ($res = $db->query($query)) {
+		$row = $res->fetch_row();
+		$yWANGateway=$row[0];
+		$res->close();
+	};
+	
+	$query="SELECT `WANGateway`, COUNT(*) FROM free GROUP BY `WANGateway` ORDER BY COUNT(*) DESC LIMIT $topWANGateway";
+	if ($res = $db->query($query)) {
+		echo "<table>";
+		printf("<tr><th>count(%s)</th><th>WANGateway (top$topWANGateway)</th></tr>\n",$yWANGateway);
+		while ($row = $res->fetch_row()) {
+			$xWANGateway=$row[0];
+			$xWANGatewaycount=$row[1];
+			printf("<tr><td>%s</td><td>%s</td></tr>\n", $xWANGatewaycount, $xWANGateway);
+		};
+		echo "</table>";
+		$res->close();
+	};
+	
+	
+	/* Таблица DNS */
+	
+	$query="SELECT COUNT(DISTINCT `DNS`) FROM `free`";
+	if ($res = $db->query($query)) {
+		$row = $res->fetch_row();
+		$yDNS=$row[0];
+		$res->close();
+	};
+	
+	$query="SELECT `DNS`, COUNT(*) FROM free GROUP BY `DNS` ORDER BY COUNT(*) DESC LIMIT $topDNS";
+	if ($res = $db->query($query)) {
+		echo "<table>";
+		printf("<tr><th>count(%s)</th><th>DNS (top$topDNS)</th></tr>\n",$yDNS);
+		while ($row = $res->fetch_row()) {
+			$xDNS=$row[0];
+			$xDNScount=$row[1];
+			printf("<tr><td>%s</td><td>%s</td></tr>\n", $xDNScount, $xDNS);
+		};
+		echo "</table>";
+		$res->close();
+	};
 ?>
