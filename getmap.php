2e6da70dd661dd19d66d3959af9b1b611722bc6e
binarymaster <x86corez@gmail.com>
2015-07-19 19:25:36 +0300

    getmap.php работа с JSON

--- getmap.php
+++ getmap.php
@@ -10,13 +10,15 @@
 	
 	$query="SELECT SQL_NO_CACHE * FROM `free` WHERE `latitude` BETWEEN $lat1 AND $lat2 AND `longitude` BETWEEN $lon1 AND $lon2 LIMIT 1000";
 
-	if ($res = $db->query($query)) {
-
+	if ($res = $db->query($query))
+	{
+		Header("Content-Type: application/json");
 		$id=0;
-		echo "$callback(";
-		while ($row = $res->fetch_row()) {
-			if ($id==0){echo '{"type": "FeatureCollection","features": [';}
-			else{echo ',';};
+		$json['type'] = 'FeatureCollection';
+		$json['features'] = array();
+		$ap['type'] = 'Feature';
+		while ($row = $res->fetch_row())
+		{
 			$xtime=$row[0];
 			$xcomment=$row[1];
 			$xbssid=$row[8];
@@ -26,14 +28,19 @@
 			$xwpspin=$row[12];
 			$xlatitude=$row[19];
 			$xlongitude=$row[20];
-			printf('{"type": "Feature","id": %s,"geometry": {"type": "Point","coordinates": [%s, %s]},"properties": {"hintContent": "%s"}}',$id,$xlatitude,$xlongitude,"$xbssid<br>$xessid<br>$xwifikey");
+
+			$ap['id'] = $id;
+			$ap['geometry']['type'] = 'Point';
+			$ap['geometry']['coordinates'][0] = (float)$xlatitude;
+			$ap['geometry']['coordinates'][1] = (float)$xlongitude;
+			$ap['properties']['hintContent'] = "$xbssid<br>$xessid<br>$xwifikey";
+
+			$json['features'][] = $ap;
 			$id++;
-		};
-		
-		if ($id>0){echo ']}';};
-		echo ')';
+		}
+		echo $callback.'('.json_encode($json).')';
 		$res->close();
-	};
+	}
 
 
 ?>
