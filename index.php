c427e88a064fd319093395e5ed62bda748864cb5
binarymaster <x86corez@gmail.com>
2015-10-04 14:50:20 +0300

    Небольшие правки дизайна

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <title>3WiFi: Свободная база точек доступа</title>
 <meta http-equiv=Content-Type content="text/html;charset=UTF-8">
 </head>
-<frameset cols="180,*">
+<frameset cols="200,*">
 	<frame src="left.html" name="leftFrame" noresize>
 	<frame src="main.html" name="mainFrame">
 </frameset>
