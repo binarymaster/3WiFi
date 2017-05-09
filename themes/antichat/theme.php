c25113f5bbb2ca740e2dbcf75a799d19194437d2
binarymaster <x86corez@gmail.com>
2017-05-09 14:46:15 +0300

    Themes: Update antichat theme

--- themes/antichat/theme.php
+++ themes/antichat/theme.php
@@ -1,5 +1,5 @@
 <?php
-$theme_data['css'] = "$theme_base/$theme/style.css?2017-03-05";
+$theme_data['css'] = "$theme_base/$theme/style.css?2017-05-09";
 $theme_data['head'] = "<div class=pageWidth><div class=logo1></div><div class=logo2><div class=logo3></div></div><span class=helper></span></div>";
 $theme_data['ajax'] = "$theme_base/$theme/img/ajax.gif";
 ?>
